c5cefe8312a696042b8499f8fedbc671562a9b9b
sayed_hossain <sayed.hasan0011@gmail.com>
2023-12-01 03:31:19 +0600

    Add a Dropdown form after the profile details table

--- src/pages/BioProfile/BioProfileTable.jsx
+++ src/pages/BioProfile/BioProfileTable.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Table } from "flowbite-react";
+import { Dropdown } from "flowbite-react";
+import { HiCog, HiCurrencyDollar, HiLogout, HiViewGrid } from "react-icons/hi";
+import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
+
 
 const BioProfileTable = ({ bio }) => {
   const {
@@ -62,14 +66,12 @@ const BioProfileTable = ({ bio }) => {
             <Table.Cell>{weight} kg</Table.Cell>
           </Table.Row>
 
-
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Present Address
             </Table.Cell>
             <Table.Cell>{present_address}</Table.Cell>
           </Table.Row>
-
 
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
@@ -78,14 +80,12 @@ const BioProfileTable = ({ bio }) => {
             <Table.Cell>{address}</Table.Cell>
           </Table.Row>
 
-
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Father's Name
             </Table.Cell>
             <Table.Cell>{father} </Table.Cell>
           </Table.Row>
-
 
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
@@ -94,14 +94,12 @@ const BioProfileTable = ({ bio }) => {
             <Table.Cell>{mother} </Table.Cell>
           </Table.Row>
 
-
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Expected Partner Age
             </Table.Cell>
             <Table.Cell>{expected_age} </Table.Cell>
           </Table.Row>
-
 
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
@@ -110,20 +108,40 @@ const BioProfileTable = ({ bio }) => {
             <Table.Cell>{expected_height} </Table.Cell>
           </Table.Row>
 
-
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               Expected Partner Weight
             </Table.Cell>
             <Table.Cell>{expected_weight} </Table.Cell>
           </Table.Row>
-
-
-
-
-
         </Table.Body>
       </Table>
+
+      {/* Request Form Start */}
+      <div className="my-5 mx-2">
+        <Dropdown label="Contact" placement="right" className="w-[400px] px-10 py-10">
+        <form className="flex max-w-md flex-col gap-4">
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="email1" value="Your email" />
+        </div>
+        <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="password1" value="Your password" />
+        </div>
+        <TextInput id="password1" type="password" required />
+      </div>
+      <div className="flex items-center gap-2">
+        <Checkbox id="remember" />
+        <Label htmlFor="remember">Remember me</Label>
+      </div>
+      <Button type="submit">Submit</Button>
+    </form>
+        </Dropdown>
+      </div>
+      {/* Request Form End */}
     </div>
   );
 };
